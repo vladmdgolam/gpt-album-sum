a4ad815a553d04e22f70ec94125c24678e69ded1
Vlad <vladmdgolam@ya.ru>
2023-02-12 03:23:21 +0400

    add credits

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,7 +22,16 @@ export default function Footer() {
           rel="noreferrer"
           className="font-bold hover:underline transition underline-offset-2"
         >
-          Vercel Edge Functions.
+          Vercel Edge Functions.{" "}
+        </a>
+        Based on{" "}
+        <a
+          href="https://vercel.com/templates/next.js/twitter-bio"
+          target="_blank"
+          rel="noreferrer"
+          className="font-bold hover:underline transition underline-offset-2"
+        >
+          AI Twitter Bio Template.
         </a>
       </div>
       <div className="flex space-x-4 pb-4 sm:pb-0">
